b7eed499a6e2c0a5005d4db013e0c93d8badc6ac
Arnut Meesut <arnut.meesut@g.swu.ac.th>
2026-04-17 05:00:51 +0100

    security lab: semgrep

---
+++ expressjs-509/controllers/categoriesController.js
@@ -0,0 +1,16 @@
+const categories = [];
+
+const getCategories = (req, res) => {
+  if (!categories || categories.length === 0) {
+    return res.json({ categories: ["unknown"] });
+  }
+
+  res.json({ categories });
+};
+
+// Implement other controllers related to categories
+
+module.exports = {
+  getCategories,
+  // Export other category controllers
+};
---
+++ expressjs-509/controllers/tagsController.js
@@ -0,0 +1,15 @@
+const tags = [];
+let counter = 0;
+
+const getTags = (req, res) => {
+  tags.push(counter);
+  counter++
+  res.json({ tags: tags.join(",")});
+};
+
+// Implement other controllers related to tags
+
+module.exports = {
+  getTags,
+  // Export other tag controllers
+};
